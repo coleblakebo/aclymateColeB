6d73d3c5258c5861c72c7f01d2fb57fef69facfb
coleblakebo <68309120+coleblakebo@users.noreply.github.com>
2021-05-04 15:03:31 -0600

    Update apiChecker.js

--- apiChecker.js
+++ apiChecker.js
@@ -1,17 +1,21 @@
 function schemaChecker(body, schema, boolArr = []){
+    //Loops through schema
     Object.keys(schema).forEach(key => {
+        //Checks if the current key is required and in body, if not pushes false
         if(typeof schema[key] === 'object'){
             if(schema[key].required){
                 if(!(key in body))
                     boolArr.push(false)
                  schemaChecker(body[key],schema[key], boolArr)
             }
+            // Checks if the types match, if not push false
             if(key in body){
                 if(schema[key].type != typeof body[key])
                     boolArr.push(false)
             }
         }
     })
+    //Checks if false was ever pushed
     if(boolArr.every(e => e != false))
         return true
     return false
